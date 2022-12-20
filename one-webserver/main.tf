1bcd8204609d121962214742d3d2e21c6efe017b
sjh9397 <sjh9397@naver.com>
2022-12-20 15:16:52 +0900

    20221220

--- one-webserver/main.tf
+++ one-webserver/main.tf
@@ -23,7 +23,7 @@ resource "aws_instance" "example" {
   user_data = <<-EOF
 					#!/bin/bash
 					echo "Hello, World" > index.html
-					nohup busybox httpd -f -p 8080 &
+					nohup busybox httpd -f -p ${var.server_port} & #보간법 
 					EOF
 
 
@@ -36,8 +36,8 @@ resource "aws_security_group" "instance" {
   name = "aws07-terrafrom-example-instance"
 # 인바운드 규칙 설정 
   ingress {
-    from_port   = 8080 #출발 포트 
-    to_port     = 8080 #도착 포트 
+    from_port   = var.server_port #출발 포트 
+    to_port     = var.sever_port #도착 포트 
     protocol    = "tcp" #프로토콜 
     cidr_blocks = ["0.0.0.0/0"] # 송신지 
   }
--- syntax/variables.tf
+++ syntax/variables.tf
@@ -1,6 +1,19 @@
-variable "number" {
-  description = " An example of a number variable in Terraform"
-  type		  = number
-  default	  = 100
+#변수지정 
+variable "number_example" {
+	type = number
+	default = 100
+}
 
-  }
+variable "list_example"{
+	description = "An example of a list in Terraform"
+	type		= list
+	default		= ["a", "b", "c"]
+}
+
+
+variable "string_example"{
+	type = string
+    default = "terraform-example-instance"
+}
+
+
